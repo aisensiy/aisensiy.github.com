41a1f1f06b2e38c663bab635e4d149cfced1189e
xushanchuan <aisensiy@163.com>
2021-07-16 22:37:27 +0800

    feat: update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,16 +5,23 @@ export default function Archive({ data }) {
   return (
     <Base>
       <div>
-        <h1 className="text-4xl font-extrabold tracking-tight my-4 text-gray-800">About</h1>
+        <h1 className="text-4xl font-extrabold tracking-tight my-4 text-gray-800">
+          About
+        </h1>
         <p>
-          实在是看不惯老版本博客模板里面对代码的处理，而且最近看到别人那种两列的
-          blog
-          非常好看，心里痒痒的终于在一天的凌晨三点找到了一个不错的免费测模板换上，感觉不错。不过之前的
-          jekyll 模板还是出了很多力的，也在此感谢之前的作者 hux。 写 blog
+          为了再次更新一下自己的前端技能，时隔多年之后又拿博客做了个测试。完成了从
+          jekyll 到 gatsby 的转换。
+        </p>
+        <p>
+          从样式上差不多就是用 tailwindcss
+          对之前样式的复制，当然目的也是为了体验下 tailwindcss 的风格。
+        </p>
+        <p>
+          写 blog
           的主要目的是用于记录自己在不同时期学到的东西（好记性不如烂笔头）以及所思所想，时不时翻翻自己以前的文章想想当时的自己也是一个很有意思的事情。当然，如果有幸能被他人发现并且其中的一些内容可以有所帮助那就再好不过了。
-          由于之前使用的 hypercomments 变成付费了，我只能换回 disqus
-          了，很遗憾，不翻墙不能评论的日子终究还是回来了。
-          如果有神马需要联系的，请发邮件给 aisensiy[AT]163.com。
+        </p>
+        <p>
+          如果有神马需要联系的，请发邮件给 <code>aisensiy[AT]163.com</code>。
         </p>
       </div>
     </Base>
