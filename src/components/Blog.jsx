61dfe4f4ee2c2bda96fd0dc26846d347769b7f28
xushanchuan <aisensiy@163.com>
2021-07-20 23:42:44 +0800

    feat:dark mode for table of content

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -14,13 +14,13 @@ export default function Blog({ data }) {
   const { frontmatter, html, fields, tableOfContents } = data;
   return (
     <div>
-      <h1 className="text-4xl font-extrabold tracking-tight my-4 text-gray-800">
+      <h1>
         {(fields && fields.slug_without_date && (
           <Link to={fields.slug_without_date}>{frontmatter.title}</Link>
         )) ||
           `${frontmatter.title}`}
       </h1>
-      <h2 className="text-gray-600 mb-4">{frontmatter.date}</h2>
+      <h2>{frontmatter.date}</h2>
       {tableOfContents && <TableOfContent html={tableOfContents} />}
       <div
         className="blog-post-content"
