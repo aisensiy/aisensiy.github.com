ebbea7be86fd8fa81b88590cb16bb1e16e060749
aisensiy <aisensiy@163.com>
2022-01-23 13:55:33 +0800

    feat: upgrade for tailwindcss typography

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -23,7 +23,7 @@ export default function Blog({ data }) {
       <h2>{frontmatter.date}</h2>
       {tableOfContents && <TableOfContent html={tableOfContents} />}
       <div
-        className="prose dark:prose-dark max-w-none lg:prose-lg xl:prose-xl"
+        className="prose prose-blog dark:prose-invert max-w-none lg:prose-lg xl:prose-xl"
         dangerouslySetInnerHTML={{ __html: html }}
       />
     </div>
